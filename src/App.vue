0cf6e3a97be59894b8db85597f382270b4bbe851
Chip Ach <chipach@gmail.com>
2020-10-21 08:56:38 -0400

    Sample app init

---
+++ src/App.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="panel-body">
+    <vue-form-generator :schema="schema" :model="model" :options="formOptions"></vue-form-generator>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+import VueFormGenerator from 'vue-form-generator'
+import 'vue-form-generator/dist/vfg.css'
+
+Vue.use(VueFormGenerator)
+
+const sample_data = false
+
+export default {
+  data () {
+    return {
+      model: {
+        id: 1,
+        name: sample_data ? 'John Doe' : null,
+        password: sample_data ? 'J0hnD03!x4' : null ,
+        skills: sample_data ?  ['Javascript', 'VueJS'] : null,
+        email: sample_data ?  'john.doe@gmail.com' : null,
+        status: sample_data ? true : false
+      },
+      schema: {
+        fields: [
+          {
+            type: 'input',
+            inputType: 'text',
+            label: 'ID (disabled text field)',
+            model: 'id',
+            readonly: true,
+            disabled: true
+          },
+          {
+            type: 'input',
+            inputType: 'text',
+            label: 'Name',
+            model: 'name',
+            placeholder: 'Your name',
+            featured: true,
+            required: true
+          },
+          {
+            type: 'input',
+            inputType: 'password',
+            label: 'Password',
+            model: 'password',
+            min: 6,
+            required: true,
+            hint: 'Minimum 6 characters',
+            validator: VueFormGenerator.validators.string
+          },
+          {
+            type: 'select',
+            label: 'Skills',
+            model: 'skills',
+            values: ['Javascript', 'VueJS', 'CSS3', 'HTML5']
+          },
+          {
+            type: 'input',
+            inputType: 'email',
+            label: 'E-mail',
+            model: 'email',
+            placeholder: 'User\'s e-mail address'
+          },
+          {
+            type: 'checkbox',
+            label: 'Status',
+            model: 'status',
+            default: true
+          },
+          {
+            type: 'submit',
+            buttonText: 'Submit'
+          }
+        ]
+      },
+      formOptions: {
+        validateAfterLoad: true,
+        validateAfterChanged: true,
+        validateAsync: true
+      }
+    }
+  }
+}
+</script>
